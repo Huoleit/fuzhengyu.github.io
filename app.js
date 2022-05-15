eb6e47b24ae266ec4b28619ae5e0f6246a9639b3
Fu Zhengyu <zhengfuaj@gmail.com>
2022-05-15 22:05:23 +0200

    add maintenance page

--- app.js
+++
@@ -1,8 +0,0 @@
-function video_start(e) {
-    // e.querySelector('.two').style.opacity = "1";
-    // e.querySelector('.three').style.opacity = "0";
-  }
-function video_stop(e) {
-    // e.querySelector('.two').style.opacity = "0";
-    // e.querySelector('.three').style.opacity = "1";
-  }
